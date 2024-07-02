bd029e74a8dc5e9e928d89065cc75d0ae7274fc7
daersh <daersh@naver.com>
2024-07-03 08:51:02 +0900

    Update SecurityConfig.java

--- src/main/java/com/daersh/daersh_project/config/SecurityConfig.java
+++ src/main/java/com/daersh/daersh_project/config/SecurityConfig.java
@@ -22,6 +22,7 @@ import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 @Configuration
@@ -57,8 +58,8 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
-//                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-                        configuration.setAllowedOrigins(Collections.singletonList("*")); // 모든 도메인 허용
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://daersh.netlify.app")); // 두 도메인 모두 허용
+//                        configuration.setAllowedOrigins(Collections.singletonList("*")); // 모든 도메인 허용
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
