7860cf8b6bfb664bd90a670fbfaba1b7739a573d
daersh <daersh@naver.com>
2024-07-02 22:38:07 +0900

    Update CorsMvcConfig.java

--- src/main/java/com/daersh/daersh_project/config/CorsMvcConfig.java
+++ src/main/java/com/daersh/daersh_project/config/CorsMvcConfig.java
@@ -7,8 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry corsRegistry){
-        corsRegistry.addMapping(("/**"))
-                .allowedOrigins("http://localhost:5173");
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("*");
     }
 }
