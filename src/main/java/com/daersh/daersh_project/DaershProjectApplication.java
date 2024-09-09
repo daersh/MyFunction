fbe03aedbbf744ff2de6c704d9f9007bc1293e8c
daersh <daersh@naver.com>
2024-09-09 18:32:06 +0900

    TimeZone 설정

--- src/main/java/com/daersh/daersh_project/DaershProjectApplication.java
+++ src/main/java/com/daersh/daersh_project/DaershProjectApplication.java
@@ -1,10 +1,23 @@
 package com.daersh.daersh_project;
 
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@Slf4j
 public class DaershProjectApplication {
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        log.info("start time: " + LocalDateTime.now());
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DaershProjectApplication.class, args);
     }
